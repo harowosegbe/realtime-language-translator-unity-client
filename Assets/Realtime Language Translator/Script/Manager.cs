b0938fd0fdc31dc24f7ad1623ef545d7155c612e
Hammed Arowosegbe <48373214+harowosegbe@users.noreply.github.com>
2024-05-13 16:08:33 +0100

    feat: Updated Unity Build

--- Assets/Realtime Language Translator/Script/Manager.cs
+++ Assets/Realtime Language Translator/Script/Manager.cs
@@ -17,19 +17,25 @@ using UnityEngine.Events;
 
 public class Manager : MonoBehaviour
 {
-
-    NRVideoCapture m_VideoCapture = null;
     NRAudioCapture m_AudioCapture = null;
 
-    [SerializeField] private RawImage m_Preview = null;
+    [Header("UI Objects")]
     [SerializeField] private TextMeshProUGUI m_DisplayText = null;
+    [SerializeField] private TMP_Dropdown m_FromLangDropdown = null;
+    [SerializeField] private TMP_Dropdown m_ToLangDropdown = null;
+    [SerializeField] private Button m_StartButton = null;
+
+    [Header("Audio")]
     [SerializeField] private AudioSource m_Audio = null;
     AudioClip m_CurrentAudio = null;
+    [SerializeField] private List<string> m_SupportedLanguages = new();
 
-    [SerializeField] private List<string> m_Translations = new List<string>();
+    [SerializeField] private List<string> m_Translations = new();
     [SerializeField] private int m_CurrentSendIndex = 0;
 
     [SerializeField] private string m_APIBaseURL = "";
+    [SerializeField] private int m_ListenDuration = 5;
+    [SerializeField] private bool m_SystemListenState = false;
 
     public string VideoSavePath
     {
@@ -47,86 +53,108 @@ public class Manager : MonoBehaviour
         public string success;
         public string translation;
     }
+
+    [System.Serializable]
+    public class AudioTranslateData
+    {
+        public string audio;
+        public string fromLang;
+        public string toLang;
+    }
  
     // Start is called before the first frame update
     void Start()
     {
-        StartRecording();
-        UpdateTextDisplay();
-        // NRInput.AddClickListener(ControllerHandEnum.)
+        // StartRecording();
+        UpdateUIDisplays();
+
+        ControllerType controllerType = NRInput.GetControllerType();
+        ControllerHandEnum domainHand = NRInput.DomainHand;
+
+        if (m_StartButton)
+        {
+            m_StartButton.onClick.AddListener(OnStartButtonClicked);
+        }
+
+        m_FromLangDropdown.options.Clear();
+        m_ToLangDropdown.options.Clear();
+
+        foreach (var item in m_SupportedLanguages)
+        {
+            m_FromLangDropdown.options.Add(
+                new TMP_Dropdown.OptionData(item)
+            );
+
+            m_ToLangDropdown.options.Add(
+                new TMP_Dropdown.OptionData(item)
+            );
+        }
+        
     }
 
-    void CreateVideoCapture()
+    private void OnStartButtonClicked()
     {
-        NRVideoCapture.CreateAsync(false,  (NRVideoCapture videoCapture)=>{
-            m_VideoCapture = videoCapture;
-        });
+        m_SystemListenState = !m_SystemListenState;
 
+        if (m_SystemListenState)
+        {
+            StartRecording();
+        }
+        else{
+            StopRecording();
+        }
+
+        UpdateUIDisplays();
+
+    }
+
+    void CreateAudioCapture()
+    {
         m_AudioCapture = NRAudioCapture.Create();
         m_AudioCapture.OnAudioData += OnAudioDataReceived;
     }
 
     void StartRecording(){
-        if (m_VideoCapture == null){
-            CreateVideoCapture();
+        if (m_AudioCapture == null)
+        {
+            CreateAudioCapture();
         }
 
-        CameraParameters cameraParameters = new()
-        {
-            cameraResolutionHeight = 200,
-            cameraResolutionWidth = 200,
-            hologramOpacity = 0.0f,
-            frameRate = 24,
-            blendMode = BlendMode.Blend,
-            // Set audio state, audio record needs the permission of "android.permission.RECORD_AUDIO",
-            // Add it to your "AndroidManifest.xml" file in "Assets/Plugin".
-            audioState = NRVideoCapture.AudioState.MicAudio,
-            camMode = CamMode.None
-        };
-        
-        StartCoroutine(AudioInput(10));
-        // m_VideoCapture.StartVideoModeAsync(cameraParameters, OnStartedVideoCaptureMode);
-        // m_AudioCapture.StartAudioModeAsync(cameraParameters, OnEndedAudioCaptureMode);
+        StartCoroutine(AudioInput(m_ListenDuration));
     }
 
-    private void OnEndedAudioCaptureMode(NRAudioCapture.AudioCaptureResult result)
-    {
-        // throw new NotImplementedException();
-        if (!result.success)
-        {
-            // NRDebugger.Info("Started Audio Capture Mode faild!");
-            return;
-        }
+    void StopRecording(){
 
-        m_AudioCapture.StartRecordingAsync(VideoSavePath, onAudioStarted );
-    
+        StopAllCoroutines();
+        EndAndSendAudioData();
     }
 
-    private void onAudioStarted(NRAudioCapture.AudioCaptureResult result)
-    {
-        
-    }
-
-    void OnStartedVideoCaptureMode(NRVideoCapture.VideoCaptureResult result)
-    {
-        if (!result.success)
-        {
-            // NRDebugger.Info("Started Audio Capture Mode faild!");
-            return;
-        }
-
-        // m_VideoCapture.GetContext().StartCapture();
-
-        StartCoroutine(AudioInput(5));        
-    }
-
-    private void UpdateTextDisplay (){
+    private void UpdateUIDisplays (){
 
         m_DisplayText.text = "";
 
-        foreach (var item in m_Translations)
+        var latestTranslations = m_Translations.TakeLast(5);
+
+        foreach (var item in latestTranslations)
         {
             m_DisplayText.text += item + " ";
+        }
+
+        var btnText = m_StartButton.GetComponentInChildren<TextMeshProUGUI>();
+
+        if (!btnText)
+        {
+            return;
+        }
+
+        if (m_SystemListenState)
+        {
+            btnText.text = "Stop Listening";
+            m_StartButton.image.color = Color.red;
+        }
+        else{
+            btnText.text = "Start Listening";
+            m_StartButton.image.color = Color.green;
         }
 
     }
@@ -141,10 +169,20 @@ public class Manager : MonoBehaviour
         //button to trigger listening
         
         m_CurrentAudio = Microphone.Start(null, false, loopTime, 44100);
-
-        //filtering
-
+        
         yield return new WaitForSeconds(10);
+        
+        EndAndSendAudioData();
+        
+        m_CurrentSendIndex++;
+        m_Translations.Add("");
+        StartCoroutine(AudioInput(m_ListenDuration));
+
+    }
+
+    private void EndAndSendAudioData(){
+
+        int translationIndex = m_CurrentSendIndex;
 
         // Stop recording
         Microphone.End(null);
@@ -152,23 +190,20 @@ public class Manager : MonoBehaviour
         // Convert audio clip to WAV format
         byte[] audioData = WavUtility.FromAudioClip(m_CurrentAudio);
 
-        // async 
-        //send to API
-        // StartCoroutine(SendAudioData(m_CurrentAudio));
+        //Send Audio to API
         StartCoroutine(SendAudioData(audioData, (text)=>{
-
-            TranslationResponse response = JsonUtility.FromJson<TranslationResponse>(text);
-
-            if (!String.IsNullOrEmpty(response.translation))
-            {
-                m_Translations.Add(response.translation);
-                UpdateTextDisplay();
-            }
-            
+            HandleResponseText(text, translationIndex);
         }));
+    }
 
-        m_CurrentSendIndex++;
-        // StartCoroutine(AudioInput(5));
+    private void HandleResponseText (string text, int index){
+        TranslationResponse response = JsonUtility.FromJson<TranslationResponse>(text);
+
+        if (!String.IsNullOrEmpty(response.translation))
+        {
+            m_Translations.Insert(index, response.translation);
+            UpdateUIDisplays();
+        }
     }
 
     IEnumerator SendAudioData(byte[] audioData, UnityAction<string> callback)
@@ -176,9 +211,17 @@ public class Manager : MonoBehaviour
 
         // Encode audio data as base64 string
         string base64Audio = System.Convert.ToBase64String(audioData);
+        
+        AudioTranslateData audioDataJson = new (){
+            audio = base64Audio,
+            fromLang = m_FromLangDropdown.options[m_FromLangDropdown.value].text,
+            toLang = m_ToLangDropdown.options[m_ToLangDropdown.value].text
+        };
+
+        string json = JsonUtility.ToJson(audioDataJson);
 
         // Create JSON object with audio data
-        string json = "{\"audio\": \"" + base64Audio + "\"}";
+        // string json = "{\"audio\": \"" + base64Audio + "\", \"lang\": \"" + m_FromLangDropdown.value + "\"}";
 
         // Replace with your server URL
         string serverURL = m_APIBaseURL + "/v1/translation";
